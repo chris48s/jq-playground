ab524c497c8df12a87f8b3027250e101ffdfb234
chris48s <git@chris-shaw.dev>
2024-03-31 18:53:24 +0100

    add curl+jq flavour, move to webpack

---
+++ src/js/app.js
@@ -0,0 +1,58 @@
+import * as curlconverter from "curlconverter";
+
+
+// jq
+const jsonDoc = document.getElementById("json-doc");
+const jqQuery = document.getElementById("jq-query");
+const jqResult = document.getElementById("jq-result");
+
+function runQuery() {
+  try {
+    const parsedJson = JSON.parse(jsonDoc.value);
+    jqResult.innerHTML =
+      "<pre><code>" +
+      JSON.stringify(jq.json(parsedJson, jqQuery.value), null, 2) +
+      "</code></pre>";
+  } catch (error) {
+    jqResult.innerHTML = "<pre><code>" + error.message + "</code></pre>";
+  }
+}
+
+jsonDoc.addEventListener("input", runQuery);
+jqQuery.addEventListener("input", runQuery);
+jq.onInitialized.addListener(runQuery);
+
+
+// curl
+const curlExecute = document.getElementById("curl-execute");
+const curlCommand = document.getElementById("curl-command");
+const curlErrors = document.getElementById("curl-errors");
+
+function runCurl() {
+  curlErrors.innerHTML = "";
+  jsonDoc.value = "";
+  runQuery();
+
+  let jsCode;
+  try {
+    jsCode = curlconverter.toJavaScript(curlCommand.value);
+  } catch (error) {
+    curlErrors.innerHTML =
+      "<h2>Errors</h2><pre><code>" + error.message + "</code></pre>";
+    return;
+  }
+  const promise = eval(jsCode);
+  promise
+    .then(async function (result) {
+      jsonDoc.value = await result.text();
+      runQuery();
+    })
+    .catch(function (error) {
+      curlErrors.innerHTML =
+        "<h2>Errors</h2><pre><code>" + error.message + "</code></pre>";
+    });
+}
+
+if (curlExecute) {
+  curlExecute.addEventListener("click", runCurl);
+}
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/js/app.js",
+  mode: "production",
+  devtool: "source-map",
+  output: {
+    filename: "js/app.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  resolve: {
+    fallback: {
+      path: require.resolve("path-browserify"),
+      fs: false,
+    },
+  },
+  experiments: {
+    topLevelAwait: true,
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        // curlconverter
+        { from: "node_modules/web-tree-sitter/tree-sitter.wasm", to: "js" },
+        // needs to be in the root :/
+        "node_modules/curlconverter/dist/tree-sitter-bash.wasm",
+
+        // jq-web
+        {
+          from: "jq.wasm.js",
+          to: "js",
+          context: "node_modules/jq-web",
+        },
+        {
+          from: "jq.wasm.wasm",
+          to: "js",
+          context: "node_modules/jq-web",
+        },
+
+        // water
+        { from: "node_modules/water.css/out/light.css", to: "css" },
+
+        // project files
+        { from: "*.html", to: "", context: "src" },
+        { from: "src/css", to: "css" },
+      ],
+    }),
+    new webpack.ProvidePlugin({
+      process: "process/browser",
+    }),
+  ],
+  // Don't warn that we have a big JS bundle.
+  performance: { hints: false },
+};
